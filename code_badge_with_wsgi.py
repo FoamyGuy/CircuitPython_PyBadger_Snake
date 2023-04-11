44aceae41b73e7655b4697930878f2d0bb10dd2d
foamyguy <foamyguy@gmail.com>
2023-04-10 19:25:03 -0500

    menu timeout, and wsgi color server version

--- code_badge_with_wsgi.py
+++ code_badge_with_wsgi.py
@@ -15,6 +15,27 @@ import board
 import supervisor
 from adafruit_pybadger import pybadger
 from displayio_listselect import ListSelect
+import busio
+import os
+from digitalio import DigitalInOut
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+import adafruit_esp32spi.adafruit_esp32spi_wsgiserver as server
+import gc
+from adafruit_wsgi.wsgi_app import WSGIApp
+
+try:
+    import json as json_module
+except ImportError:
+    import ujson as json_module
+
+from simple_wsgi_application import SimpleWSGIApplication
+
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
 
 display = board.DISPLAY
 
@@ -24,6 +45,9 @@ MENU_ITEMS = ["Snake", "IR Cam", "Back"]
 MENU_IDLE_TIMEOUT = 10  # secconds
 MENU_SHOWING = False
 MENU_START_TIME = None
+
+LAST_IO_SYNC_TIME = -1
+IO_SYNC_DELAY = 0.5
 
 SELECT_RELEASED = True
 
@@ -37,7 +61,62 @@ pybadger.show_badge(
     name_string="Blinka", hello_scale=2, my_name_is_scale=2, name_scale=3
 )
 
+esp32_cs = DigitalInOut(board.D13)
+esp32_ready = DigitalInOut(board.D11)
+esp32_reset = DigitalInOut(board.D12)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+"""Use below for Most Boards"""
+# status_light = neopixel.NeoPixel(
+#     board.NEOPIXEL, 1, brightness=0.2
+# )  # Uncomment for Most Boards
+
+
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets)
+
+# Connect to WiFi
+print("Connecting to WiFi...")
+wifi.connect()
+print("Connected!")
+
+web_app = WSGIApp()
+
+f = open("static/index.html")
+index_str = f.read()
+f.close()
+
+@web_app.route("/")
+def index(request):
+
+    return ("200 OK", [], index_str)
+
+@web_app.route("/led_on/<r>/<g>/<b>")
+def led_on(request, r, g, b):  # pylint: disable=unused-argument
+    print("led on!")
+    pybadger.pixels.fill((int(r), int(g), int(b)))
+    return ("200 OK", [], index_str)
+
+
+@web_app.route("/led_off")
+def led_off(request):  # pylint: disable=unused-argument
+    print("led off!")
+    pybadger.pixels.fill(0)
+    return ("200 OK", [], index_str)
+
+
+# Here we setup our server, passing in our web_app as the application
+server.set_interface(esp)
+wsgiServer = server.WSGIServer(80, application=web_app)
+
+print("open this IP in your browser: ", esp.pretty_ip(esp.ip_address))
+
+# print(esp.get_time())
+# Start the server
+wsgiServer.start()
+
 while True:
+
     pybadger.auto_dim_display(
         delay=10
     )  # Remove or comment out this line if you have the PyBadge LC
@@ -50,7 +129,8 @@ while True:
             email_string_two="adafruit.com",
         )
     elif pybadger.button.b:
-        pybadger.show_qr_code(data="https://circuitpython.org")
+        pybadger.show_qr_code(data=f"http://{esp.pretty_ip(esp.ip_address)}")
+        #pybadger.show_qr_code(data="https://circuitpython.org")
     elif pybadger.button.start:
         pybadger.show_badge(
             name_string="Blinka", hello_scale=2, my_name_is_scale=2, name_scale=3
@@ -89,6 +169,23 @@ while True:
         SELECT_RELEASED = True
 
     if MENU_SHOWING:
-        if MENU_START_TIME + MENU_IDLE_TIMEOUT > time.monotonic():
+        if MENU_START_TIME + MENU_IDLE_TIMEOUT <= time.monotonic():
+            print("Menu Idle Timeout")
             pybadger.show(previous_group)
             MENU_SHOWING = False
+
+    if LAST_IO_SYNC_TIME + IO_SYNC_DELAY <= time.monotonic():
+
+        try:
+            gc.collect()
+            #print(f"free mem: {gc.mem_free()}")
+            wsgiServer.update_poll()
+
+            # Could do any other background tasks here, like reading sensors
+        except OSError as e:
+            print("Failed to update server, restarting ESP32\n", e)
+            wifi.reset()
+            continue
+
+
+        LAST_IO_SYNC_TIME = time.monotonic()
